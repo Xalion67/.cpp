06cda280edd48c8276ee070413447fb9a39859c1
Kostya Parfenov <xalion67@gmail.com>
2018-02-23 18:47:37 +0300

    This function determines the type system

---
+++ typeSystem.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  24
+//
+//  Created by Парфенов Константин on 25.01.2018.
+//  Copyright © 2018 Парфенов Константин. All rights reserved.
+//
+
+#include <stdio.h>
+#include <curses.h>
+unsigned short x = 1; /* 0x0001 */
+int main(void) {
+    printf("%s\n", *((unsigned char *) &x) == 0 ? "Your system: big-endian" : "Your system: little-endian");
+    return 0;
+}
