fa034cc907533c0f5c242a01518dd0312f4c21ad
Kostya Parfenov <xalion67@gmail.com>
2018-02-23 18:52:58 +0300

    this functions determines the type system

--- typeSystem.cpp
+++ typeSystem.cpp
@@ -8,7 +8,9 @@
 
 #include <stdio.h>
 #include <curses.h>
+
 unsigned short x = 1; /* 0x0001 */
+
 int main(void) {
     printf("%s\n", *((unsigned char *) &x) == 0 ? "Your system: big-endian" : "Your system: little-endian");
     return 0;
